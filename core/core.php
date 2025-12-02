322b8a991f2503479a8589301194a5d910dcd710
Jesse Nickles <jesse@littlebizzy.com>
2025-12-02 10:54:49 +0700

    Update core.php

--- core/core.php
+++ core/core.php
@@ -33,8 +33,9 @@ class EXPDBS_Core {
 	public static function instance() {
 
 		// Check instance
-		if (!isset(self::$instance))
+		if ( ! isset( self::$instance ) ) {
 			self::$instance = new EXPDBS_Core;
+		}
 
 		// Done
 		return self::$instance;
@@ -48,21 +49,16 @@ class EXPDBS_Core {
 	private function __construct() {
 
 		// Check AJAX mode
-		if (defined('DOING_AJAX') && DOING_AJAX) {
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 
-			// Check the AJAX action
-			$action = !empty($_POST['action']) ? sanitize_text_field( wp_unslash( $_POST['action'] ) ) : '';
-
-			if (0 === strpos($action, 'expdbs_')) {
-				require_once(EXPDBS_PATH.'/core/ajax.php');
-				EXPDBS_Core_AJAX::instance();
-			}
+			require_once EXPDBS_PATH . '/core/ajax.php';
+			EXPDBS_Core_AJAX::instance();
 
 		// Just the admin
 		} else {
 
 			// Admin menu handler
-			add_action('admin_menu', array($this, 'admin_menu'));
+			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		}
 	}
 
@@ -77,15 +73,16 @@ class EXPDBS_Core {
 	 * Set the admin menu
 	 */
 	public function admin_menu() {
+
 		$hook_suffix = add_management_page(
 			'Export Database',
 			'Export Database',
 			'manage_options',
 			'export-database',
-			array($this, 'admin_page')
+			array( $this, 'admin_page' )
 		);
 
-		add_action('load-' . $hook_suffix, array($this, 'admin_assets'));
+		add_action( 'load-' . $hook_suffix, array( $this, 'admin_assets' ) );
 	}
 
 
@@ -94,7 +91,8 @@ class EXPDBS_Core {
 	 * Load the admin page assets
 	 */
 	public function admin_assets() {
-		require_once(EXPDBS_PATH.'/admin/admin.php');
+
+		require_once EXPDBS_PATH . '/admin/admin.php';
 		EXPDBS_Admin::instance();
 	}
 
@@ -104,7 +102,8 @@ class EXPDBS_Core {
 	 * Show the admin page
 	 */
 	public function admin_page() {
-		require_once(EXPDBS_PATH.'/admin/admin.php');
+
+		require_once EXPDBS_PATH . '/admin/admin.php';
 		EXPDBS_Admin::instance()->view();
 	}
 
