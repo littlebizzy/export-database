d4107f94288bbe821fb7c60fc69cc71f4a7604e0
Jesse Nickles <jesse@littlebizzy.com>
2025-12-02 10:59:57 +0700

    Update ajax.php

--- core/ajax.php
+++ core/ajax.php
@@ -40,8 +40,9 @@ class EXPDBS_Core_AJAX {
 	public static function instance() {
 
 		// Check instance
-		if (!isset(self::$instance))
+		if ( ! isset( self::$instance ) ) {
 			self::$instance = new EXPDBS_Core_AJAX;
+		}
 
 		// Done
 		return self::$instance;
@@ -55,13 +56,10 @@ class EXPDBS_Core_AJAX {
 	private function __construct() {
 
 		// Run requested method
-		$action = !empty($_POST['action']) ? sanitize_text_field( wp_unslash($_POST['action']) ) : '';
+		$method = substr( $_POST['action'], 7 );
 
-		$method = substr($action, 7);
-		if (!empty($method) && method_exists($this, $method)) {
-
-			// Call the method
-			add_action('wp_ajax_' . $action, array($this, $method));
+		if ( method_exists( $this, $method ) ) {
+			add_action( 'wp_ajax_' . $_POST['action'], array( $this, $method ) );
 		}
 	}
 
@@ -81,19 +79,23 @@ class EXPDBS_Core_AJAX {
 		$this->load_export();
 
 		// Check folder
-		if (!EXPDBS_Core_Export::check_migrations_folder($folder))
-			$this->error_ajax_response('Cannot create the folder: '.$folder);
+		if ( ! EXPDBS_Core_Export::check_migrations_folder( $folder ) ) {
+			$this->error_ajax_response( 'Cannot create the folder: ' . $folder );
+		}
 
 		// Compression
-		$compress = !empty($_POST['compress']);
+		$compress = ! empty( $_POST['compress'] );
 
 		// Start the migration
-		if (false === ($key = EXPDBS_Core_Export::start($compress)))
-			$this->error_ajax_response('Cannot write the file.');
+		$key = EXPDBS_Core_Export::start( $compress );
+		if ( false === $key ) {
+			$this->error_ajax_response( 'Cannot write the file.' );
+		}
 
 		// Set key and show response
 		$this->response['data']['is_done'] = false;
-		$this->response['data']['key'] = $key;
+		$this->response['data']['key']     = $key;
+
 		$this->output_ajax_response();
 	}
 
@@ -108,37 +110,36 @@ class EXPDBS_Core_AJAX {
 		$this->load_export();
 
 		// Check key argument
-		if (empty($_POST['key']) || 32 != strlen($_POST['key']))
-			$this->error_ajax_response('Error in key argument.');
+		if ( empty( $_POST['key'] ) || 32 != strlen( $_POST['key'] ) ) {
+			$this->error_ajax_response( 'Error in key argument.' );
+		}
 
 		// Check export process
-		if (false === ($migration = EXPDBS_Core_Export::export($_POST['key'])))
-			$this->error_ajax_response('Error when exporting data.');
+		$migration = EXPDBS_Core_Export::export( $_POST['key'] );
+		if ( false === $migration ) {
+			$this->error_ajax_response( 'Error when exporting data.' );
+		}
 
 		// Default response
 		$this->response['data']['is_done'] = false;
 
 		// Check generation end
-		if ($migration['total']['done']) {
+		if ( $migration['total']['done'] ) {
 
-			// Check compression
-			if ($migration['zip'] || $migration['gzip']) {
+			if ( $migration['zip'] || $migration['gzip'] ) {
 				$this->response['data']['compressing'] = true;
 
-			// Is done
 			} else {
-
 				// File generated
 				$this->response['data']['is_done'] = true;
 			}
 
-		// Continue
 		} else {
 
-			// Set key and show response
-			$this->response['data']['percent'] =
-				empty($migration['total']['rows']) ? 0 :
-				round(($migration['total']['index'] / $migration['total']['rows']) * 100);
+			// Progress
+			$this->response['data']['percent'] = empty( $migration['total']['rows'] )
+				? 0
+				: round( ( $migration['total']['index'] / $migration['total']['rows'] ) * 100 );
 		}
 
 		// Output
@@ -156,17 +157,21 @@ class EXPDBS_Core_AJAX {
 		$this->load_export();
 
 		// Check key argument
-		if (empty($_POST['key']) || 32 != strlen($_POST['key']))
-			$this->error_ajax_response('Error in key argument.');
+		if ( empty( $_POST['key'] ) || 32 != strlen( $_POST['key'] ) ) {
+			$this->error_ajax_response( 'Error in key argument.' );
+		}
 
 		// Check compression process
-		if (false === ($migration = EXPDBS_Core_Export::compress($_POST['key'])))
-			$this->error_ajax_response('Error when compressing data.');
+		$migration = EXPDBS_Core_Export::compress( $_POST['key'] );
+		if ( false === $migration ) {
+			$this->error_ajax_response( 'Error when compressing data.' );
+		}
 
-		// Compressed
 		$this->response['data']['is_done'] = true;
+
 		$this->output_ajax_response();
 	}
+
 
 
 	/**
@@ -178,11 +183,12 @@ class EXPDBS_Core_AJAX {
 		$this->load_export();
 
 		// Check key value
-		if (empty($_POST['key']) || 32 != strlen($_POST['key']))
-			wp_die('Not valid key');
+		if ( empty( $_POST['key'] ) || 32 != strlen( $_POST['key'] ) ) {
+			wp_die( 'Not valid key' );
+		}
 
 		// Download file
-		EXPDBS_Core_Export::download($_POST['key']);
+		EXPDBS_Core_Export::download( $_POST['key'] );
 	}
 
 
@@ -201,27 +207,31 @@ class EXPDBS_Core_AJAX {
 		$this->check_ajax_submit();
 
 		// Load export class
-		require_once(EXPDBS_PATH.'/core/export.php');
+		require_once EXPDBS_PATH . '/core/export.php';
 	}
 
 
 
 	/**
-	 * Check and initialize ajax response
+	 * Check and initialize ajax respose
 	 */
 	private function check_ajax_submit() {
 
-		// Check default output
+		// Default response
 		$this->response = $this->default_ajax_response();
 
 		// Check user capabilities
-		if (!current_user_can('export')) {
+		if ( ! current_user_can( 'manage_options' ) ) {
 			$this->response['status'] = 'error';
 			$this->response['reason'] = 'Operation not allowed for the current user.';
 			$this->output_ajax_response();
+		}
 
-		// Check if submitted nonce matches with the generated nonce we created earlier
-		} elseif (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], EXPDBS_FILE)) {
+		// Nonce check
+		if (
+			! isset( $_POST['nonce'] ) ||
+			! wp_verify_nonce( $_POST['nonce'], EXPDBS_FILE )
+		) {
 			$this->response['status'] = 'error';
 			$this->response['reason'] = 'Security verification error: please reload this page and try again.';
 			$this->output_ajax_response();
@@ -233,9 +243,8 @@ class EXPDBS_Core_AJAX {
 	/**
 	 * Return array of ajax response
 	 */
-	private function default_ajax_response($nonce_seed = null) {
+	private function default_ajax_response( $nonce_seed = null ) {
 
-		// Default response
 		return array(
 			'status' => 'ok',
 			'reason' => '',
@@ -248,10 +257,12 @@ class EXPDBS_Core_AJAX {
 	/**
 	 * Custom error ajax response
 	 */
-	private function error_ajax_response($reason) {
-		$this->response = $this->default_ajax_response();
-		$this->response['status'] = 'error';
-		$this->response['reason'] = $reason;
+	private function error_ajax_response( $reason ) {
+
+		$this->response            = $this->default_ajax_response();
+		$this->response['status']  = 'error';
+		$this->response['reason']  = $reason;
+
 		$this->output_ajax_response();
 	}
 
@@ -261,8 +272,9 @@ class EXPDBS_Core_AJAX {
 	 * Output AJAX in JSON format and exit
 	 */
 	private function output_ajax_response() {
-		@header('Content-Type: application/json');
-		die(@json_encode($this->response));
+
+		@header( 'Content-Type: application/json' );
+		die( wp_json_encode( $this->response ) );
 	}
 
 
