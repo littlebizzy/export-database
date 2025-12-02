6644bd9188c37aaa831ca0e1d7cf0a415e0336d9
Jesse Nickles <jesse@littlebizzy.com>
2025-12-02 17:05:46 +0700

    Update admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -1,95 +1,34 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($){
 
-	$('#expdbs-export').click(function () {
+    // start export
+    $('#expdbs-export').click(function(){
+        $('#expdbs-status').html('exporting...');
+        $.post(expdbs_ajax.ajax_url, {
+            action: 'expdbs_export',
+            nonce: expdbs_ajax.nonce,
+            compress: $('#expdbs-compress').is(':checked') ? 1 : 0
+        }, function(resp){
+            if (!resp || !resp.success) {
+                alert('export failed');
+                return;
+            }
+            $('#expdbs-status').html('export completed');
+            location.reload();
+        });
+    });
 
-		$('.expdbs-view').hide();
-		$('#expdbs-init').show();
-
-		ajax_submit({
-			'start': true,
-			'done': false,
-			'action': 'expdbs_start',
-			'compress': $("#expdbs-compress").is(':checked') ? 1 : 0,
-			'nonce': $(this).attr('data-nonce')
-		});
-
-		return false;
-	});
-
-
-	function ajax_submit(data) {
-
-		$.post(get_ajax_url(), data, function (e) {
-
-			// handle fatal wp_die() HTML output
-			if (typeof e !== 'object') {
-				let msg = $('<div>').html(e).text(); // strip HTML tags
-				alert(msg || 'Server error');
-				return;
-			}
-
-			if (typeof e.status === 'undefined') {
-
-				alert('Unknown error');
-
-			} else if (e.status === 'error') {
-
-				alert(e.reason);
-
-			} else if (e.status === 'ok') {
-
-				if (e.data.is_done) {
-
-					$('#expdbs-gen').hide();
-					$('#expdbs-comp').hide();
-					$('#expdbs-done').show();
-
-					data['done'] = true;
-					data['action'] = 'expdbs_download';
-
-					$('<form action="' + get_ajax_url() + '" method="post">' +
-						'<input type="hidden" name="key" value="' + data['key'] + '" />' +
-						'<input type="hidden" name="action" value="expdbs_download" />' +
-						'<input type="hidden" name="nonce" value="' + data['nonce'] + '" />' +
-						'</form>').appendTo('body').submit();
-
-				} else if (!data['done']) {
-
-					if (data['start']) {
-
-						$('#expdbs-init').hide();
-						$('#expdbs-gen-percent').html('0');
-						$('#expdbs-gen').show();
-
-						data['start'] = false;
-						data['key'] = e.data.key;
-
-					} else if (e.data.compressing) {
-
-						$('#expdbs-gen').hide();
-						$('#expdbs-comp').show();
-
-					} else {
-
-						$('#expdbs-gen-percent').html(e.data.percent);
-					}
-
-					data['action'] = e.data.compressing ? 'expdbs_compress' : 'expdbs_export';
-					ajax_submit(data);
-				}
-			}
-
-		}).fail(function (xhr) {
-
-			// improved fail handler for fatal errors
-			let msg = xhr.responseText ? $('<div>').html(xhr.responseText).text() : '';
-			alert(msg || 'Server communication error.\nPlease try again.');
-		});
-	}
-
-
-	function get_ajax_url() {
-		return ajaxurl + '?_=' + new Date().getTime();
-	}
+    // delete file
+    $('.expdbs-delete').click(function(){
+        if (!confirm('delete this file?')) return;
+        var file = $(this).data('file');
+        $.post(expdbs_ajax.ajax_url, {
+            action: 'expdbs_delete',
+            nonce: expdbs_ajax.nonce,
+            file: file
+        }, function(resp){
+            if (resp && resp.success) location.reload();
+            else alert('unable to delete file');
+        });
+    });
 
 });
